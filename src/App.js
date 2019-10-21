7a35ccdf2b6e924c577286d9858a23489ca60d2e
Joe Denson <joedenson79@gmail.com>
2019-10-20 22:38:04 -0700

    setting up components

--- src/App.js
+++ src/App.js
@@ -1,13 +1,29 @@
 import React from 'react';
 import './App.css';
+import 'bootstrap/dist/css/bootstrap.min.css';
+import Navbar from './components/layouts/Navbar';
+import Footer from './components/layouts/Footer';
+// React Router Import
+
+import { Switch, Route } from 'react-router-dom';
+
 
 
 class App extends React.Component {
   render() {
     return (
       <div>
-        <h1>City Guide App</h1>
-        </div>
+        <Navbar />
+        <Switch>
+          <Route path='/home' component={Home} />
+          <Route path='/news' component={News} />
+          <Route path='/contacts' component={Contact} />
+          <Route path='/details' component={Details} />
+          <Route path='/page-not-found' component={NotFoundPage} />
+        </Switch>
+        <Footer />
+
+      </div>
     );
   }
 }
---
+++ src/components/pages/NotFoundPage.js
@@ -0,0 +1,16 @@
+import React, { Component } from 'react';
+
+
+
+
+class NotFoundPage extends Component {
+    render() {
+        return (
+            <div>
+                <h2>Page Not Found</h2>
+            </div>
+        )
+    }
+}
+
+export default NotFoundPage;
